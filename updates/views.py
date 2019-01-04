7053859b3ff94bfe767a23701746be9db37ccdf4
Arabyan <grigorarabyan@yahoo.com>
2019-01-05 00:57:07 +0400

    JSON CBV

--- updates/views.py
+++ updates/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
-# Create your views here.
+import json
 
 from .models import Update
 
@@ -8,12 +8,15 @@ from .models import Update
     # return render(request, template, {}) # render JSon data --> JS object Notation
     # return HttpResponse(get_template().render({}))
 
-def update_model_detaiL_view(request):
+def json_example_view(request):
     """
     URI - for a REST API
+    GET - Retrive
     """
     data = {
         "count": 1000,
         "content":"Some new content"
     }
-    return JsonResponse(data)
+    json_data = json.dumps(data)
+    # return JsonResponse(data)
+    return HttpResponse(json_data, content_type='application/json')
