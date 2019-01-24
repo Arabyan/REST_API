c05e575b2246afeae4efb110f03ca4f21b0405b0
Arabyan <grigorarabyan@yahoo.com>
2019-01-24 22:16:36 +0400

    WHOLE NEW Series of tutorials Model Form for validation

--- grgapi/settings.py
+++ grgapi/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # local
     'updates',
+    # third party
+    'rest_framework',
+    'status',
 ]
 
 MIDDLEWARE = [
