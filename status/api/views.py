8380ebc3d0d0ca04acbbc9163666712ff3e1b6a9
Arabyan <grigorarabyan@yahoo.com>
2019-02-04 02:23:58 +0400

    List & Search API View

--- status/api/views.py
+++ status/api/views.py
@@ -1,3 +1,5 @@
+from rest_framework import generics
+# from rest_framework.generics import List
 from rest_framework.views import APIView
 from rest_framework.response import Response
 # from django.views.generic import View
@@ -18,3 +20,20 @@ class StatusListSearchAPIView(APIView):
         qs = Status.objects.all()
         serializer = StatusSerializer(qs, many=True)
         return Response(serializer.data)
+
+
+class StatusAPIView(generics.ListAPIView):
+    permission_classes = []
+    authentication_classes = []
+
+    serializer_class      = StatusSerializer
+
+    def get_queryset(self):
+        qs =  Status.objects.all()
+        query = self.request.GET.get('q')
+        if query is not None:
+            qs = qs.filter(content__icontains=query)
+        return qs
+# class StatusCreateView(createView):
+#     queryset             = Status. objects.all()
+#     from_class =
